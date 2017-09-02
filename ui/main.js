6d4efef213e94ed5984613d71e6c3ac0ef3712d8
Laxmitrivenipola <31175681+Polalaxmitriveni@users.noreply.github.com>
2017-09-02 17:47:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
 console.log('Loaded!');
-var element=document.getElementById('main text');
-element.innerHTML='TRIVEE';
-var img=document.getElementById('madi');
-var marginLeft=0;
+var element = document.getElementById('main text');
+element.innerHTML = 'TRIVEE';
+var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight () {
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft +'px';
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft +'px';
 }
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+img.onclick = function () {
+    var interval = setInterval(moveRight,50);
 
 };
